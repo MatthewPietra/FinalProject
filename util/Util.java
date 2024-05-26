dadab7d8349fad261a5b46a614c46ddcdb993291
MatthewPietra <pietracupamatthew@gmail.com>
2024-05-26 17:55:08 -0400

    Added documentation for toTitleCase

--- util/Util.java
+++ util/Util.java
@@ -1,6 +1,13 @@
 package util;
 
 public class Util {
+    /**
+     * Converts each word in a string to title case.
+     * Title case means that the first letter of each word is capitalized,
+     * while all other letters are converted to lowercase
+     * @param strIn the input string containing two words separated by a single space
+     * @return the input string converted to title case
+     */
     public static String toTitleCase(String strIn) {
         String[] words = strIn.split(" ");
 
